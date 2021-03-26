e1b17cc445cb974209e4831f862dcce1ce336a73
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-03-26 10:08:31 -0300

    Campgrounds Implementation
    - Added campground details page
    - Added "add campground" page
    - Added basic navigation

--- app.js
+++ app.js
@@ -20,18 +20,30 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.render('home');
 })
 
-app.get('/makeCampground', async (req, res) => {
-    const camp = new Campground({
-        title: 'Posto de Saúde',
-        description: 'Postinho aqui do lado de casa'
-    });
+app.get('/campgrounds', async(req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds});
+})
+
+app.get('/campgrounds/new', async(req, res) => {
+    res.render('campgrounds/new');
+});
+
+app.get('/campgrounds/:id', async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', {campground});
+});
+
+app.post('/campgrounds', async (req, res) => {
+    const camp = new Campground(req.body.campground);
     await camp.save();
-    res.send(camp);
+    res.redirect(`/campgrounds/${camp._id}`);
 });
 
 app.listen(3000, () => {
