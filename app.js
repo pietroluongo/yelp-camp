89c21560f12329ca3267b46228d3c77f021a8abb
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-03-30 10:57:15 -0300

    Implemented review deletion

--- app.js
+++ app.js
@@ -103,6 +103,13 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res)
     res.redirect(`/campgrounds/${camp._id}`);
 }));
 
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(req.params.reviewId);
+    res.redirect(`/campgrounds/${id}/`);
+}));
+
 // Error handling
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
