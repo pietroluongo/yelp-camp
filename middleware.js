190039fdf42b3fb8dd72bb0b5f5e19a8f4cf170f
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-06 08:37:42 -0300

    Added automatic redirect on login

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 module.exports.isLoggedIn = (req, res, next) => {
-    console.log(`REQ.USER... ${req.user}`);
     if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl;
         req.flash('error', 'You must be logged in to do that.');
         return res.redirect('/login');
     }
--- routes/users.js
+++ routes/users.js
@@ -44,7 +44,9 @@ router.post(
         failureRedirect: '/login' }),
     (req, res) => {
         req.flash('success', 'Welcome back!');
-        res.redirect('/campgrounds');
+        const redirectUrl = req.session.returnTo || '/campgrounds';
+        delete req.session.returnTo;
+        res.redirect(redirectUrl);
     }
 );
 
