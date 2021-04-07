517ef05e4fe2fd1d1e66e72c808135e92d9334a8
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-07 11:46:51 -0300

    WIP: Cluster map implementation

--- .eslintrc.js
+++ .eslintrc.js
@@ -82,7 +82,7 @@ module.exports = {
         'max-depth': 'error',
         'max-len': 'off',
         'max-lines': 'error',
-        'max-lines-per-function': 'error',
+        'max-lines-per-function': 'off',
         'max-nested-callbacks': 'error',
         'max-params': 'off',
         'max-statements': 'off',
