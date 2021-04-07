a8c4491c757db513a3c297bae9180988b56c8f7a
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-07 09:13:02 -0300

    Updated seeds for image uploads, updated campgrounds index to show uploaded images

--- seeds/index.js
+++ seeds/index.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 const cities = require('./cities');
 const { descriptors, places } = require('./seedHelpers');
 const Campground = require('../models/Campground');
@@ -26,7 +29,16 @@ const seedDB = async() => {
         const camp = new Campground({
             location: `${cities[rand1k].city}, ${cities[rand1k].state}`,
             title: `${getRandomizedItemFromArray(descriptors)} ${getRandomizedItemFromArray(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dq6kbbbza/image/upload/v1617796268/YelpCamp/wv9ouza71vkjmzhmtyl9.png',
+                    filename: 'YelpCamp/wv9ouza71vkjmzhmtyl9'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dq6kbbbza/image/upload/v1617796268/YelpCamp/jovlaoxs4dpzm9vmcihy.png',
+                    filename: 'YelpCamp/jovlaoxs4dpzm9vmcihy'
+                }
+            ],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             price: Math.floor(Math.random() * 1000),
             author: '606c3ca0497e9211208aabf2'
